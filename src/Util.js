1031f9acc08c64019819ef13e994a5d61f88b3d3
HansLanda14ib <bibzazne@gmail.com>
2023-08-24 14:59:02 +0100

    add meta line

--- src/Util.js
+++ src/Util.js
@@ -58,10 +58,9 @@ const Util = () => {
     };
 
     function parseJwt(token) {
-        const base64Url = token.split('.')[1];
+        const base64Url = token?.split('.')[1];
         const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        const decoded = JSON.parse(atob(base64));
-        return decoded;
+        return JSON.parse(atob(base64));
     }
 
     return (
