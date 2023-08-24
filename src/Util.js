8a451b057f2fa0f64f07e4907561018ec7d7c565
HansLanda14ib <bibzazne@gmail.com>
2023-08-24 15:00:51 +0100

    add ?

--- src/Util.js
+++ src/Util.js
@@ -59,7 +59,7 @@ const Util = () => {
 
     function parseJwt(token) {
         const base64Url = token?.split('.')[1];
-        const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        const base64 = base64Url?.replace(/-/g, '+').replace(/_/g, '/');
         return JSON.parse(atob(base64));
     }
 
